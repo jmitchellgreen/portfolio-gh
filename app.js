ccb2794f4d790be6e9088260c071f131ba898c0e
jmitchellgreen <58088351+jmitchellgreen@users.noreply.github.com>
2022-08-25 18:20:51 -0400

    update routing for weather4you

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ const port = 3000
 const path = require('path')
 
 app.use(express.static(__dirname + '/site'));
-app.use(express.static(__dirname + '/site/files/weather4you/build/'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/site/index.html')
 })
 
+app.use(express.static(__dirname + '/site/files/weather4you/build/'));
+
 app.get('/weather4you', (req, res) => {
   res.sendFile(__dirname + '/site/files/weather4you/build/index.html')
 })
 
+app.get('/test', (req, res) => {
+  res.send("hello world")
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
