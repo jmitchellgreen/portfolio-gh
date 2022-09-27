d710063637de4e25035c688ec5ddb5a2436ef8db
Mitchell <mtchlgrn@gmail.com>
2022-09-27 17:09:11 -0400

    add posts.html, to be continued on production

--- site/v2/app.py
+++ site/v2/app.py
@@ -11,3 +11,13 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/posts")
+def posts():
+    return render_template("posts.html")
+
+
+@app.route("/posts/<post_title>")
+def post(post_title):
+    return render_template(post_title)
