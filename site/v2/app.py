ae7f65cea9ae76199f7a98ae769abc288da6b748
Mitchell <mtchlgrn@gmail.com>
2022-09-29 13:15:25 -0400

    found path forward for routing

--- site/v2/app.py
+++ site/v2/app.py
@@ -1,6 +1,23 @@
-from flask import Flask
-from blueprint import bp
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="pages")
 
 
-app = Flask(__name__)
-app.register_blueprint(bp)
+@app.route("/")
+def index():
+    return render_template("index/index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about/about.html")
+
+
+@app.route("/archive")
+def posts():
+    return render_template("archive/archive.html")
+
+
+@app.route("/archive/<post_title>")
+def post(post_title):
+    return render_template(post_title)
