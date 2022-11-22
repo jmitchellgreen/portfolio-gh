9de602fb505e285a851849885a6478379d355b30
jmitchellgreen <58088351+jmitchellgreen@users.noreply.github.com>
2022-11-22 12:30:50 -0500

    Fix tags for archive page

--- archive.py
+++ archive.py
@@ -11,12 +11,22 @@ def archive():
     my_archive = []
     for file in files:
         with open(f"./pages/archive/posts/{file}") as f:
+
+            # soup for each .html
             soup = BeautifulSoup(f, features="html.parser")
+
+            # parse and cleanup tags to list
+            all_tags = soup.h4.text
+            all_tags = all_tags.lstrip("[")
+            all_tags = all_tags.rstrip("]")
+            tags = all_tags.split(",")
+            tags = [x.strip() for x in tags]
+
             archive_obj = {
                 "url": file[:-5],
                 "title": soup.h1.text,
                 "thumbnail": "static/" + soup.img["src"][31:-5],
-                "tags": soup.h4.text,
+                "tags": tags,
             }
             my_archive.append(archive_obj)
 
